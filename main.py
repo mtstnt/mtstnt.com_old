caf6f3dabdc53821f673f29ede94790f4eb145ba
mtstnt <matthew.sutanto0612@yahoo.com>
2023-03-27 16:21:20 +0700

    refinishing lol

--- main.py
+++ main.py
@@ -1,41 +1,104 @@
+from dataclasses import dataclass
+from datetime import datetime
+from math import ceil
 import os
+from re import search
 import arrow
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import frontmatter
+from pathlib import Path
+
+POST_COUNT_PER_PAGE = 10
 
 app = Flask(__name__,
             static_url_path="",
             static_folder='public')
 
+def get_value_or(val, alternative) -> any:
+    return val if val else alternative
+
+@dataclass
+class Post:
+    slug: str
+    title: str
+    description: str = ""
+    excerpt: str = ""
+    keywords: str = ""
+    is_draft: bool = False
+    published_at: datetime = datetime.now()
+
+    @staticmethod
+    def from_frontmatter(post: frontmatter.Post, filename: str) -> 'Post':
+        return Post(
+            slug=filename,
+            title=post.get('title', ''),
+            description=post.get('description', ''),
+            excerpt=post.get('excerpt', ''),
+            keywords=post.get('keywords', ''),
+            is_draft=post.get('is_draft', False),
+            published_at=post.get('published_at', None),
+        )
+    
+# 1 - 10
+# 11 - 20 
+# 21 - 30
+# 31 - 40
+def get_posts(path: str, offset: int = 0, count: int = 10, search_query: str = '') -> Post:
+    posts, entries = [], sorted(Path(path).iterdir(), key=os.path.getmtime)
+    for entry in entries:
+        posts.append(Post.from_frontmatter(frontmatter.load(str(entry)), entry.name.removesuffix('.md')))
+    if search_query != '':
+        search_query = search_query.lower()
+        posts = filter(
+                lambda p: (
+                    p.title.lower().find(search_query) != -1 or
+                    p.slug.lower().find(search_query) != -1 
+                ), posts)
+
+    return list(posts)[(offset - 1):(offset - 1)+count]
+
 @app.get('/')
 def home():
+    page = request.args.get('p', 1)
+    query = request.args.get('q', '')
+    posts = get_posts('./posts', (int(page) - 1) * POST_COUNT_PER_PAGE + 1, POST_COUNT_PER_PAGE, query)
     context = {
-        'page': 'home'
+        'page': 'blog',
+        'posts': posts,
+        'query': query,
+        'prev_link': (f'/?p={int(page) - 1}' 
+                      if int(page) > 1 
+                      else None),
+        'next_link': (f'/?p={int(page) + 1}' 
+                      if int(page) != ceil(len(os.listdir('./posts')) / POST_COUNT_PER_PAGE) 
+                      else None),
     }
     return render_template('index.html', **context)
 
-@app.get('/contact')
+@app.get('/resume')
 def contact():
     context = {
-        'page': 'contact'
+        'page': 'resume'
     }
-    return render_template('contact.html', **context)
+    return render_template('resume.html', **context)
 
-@app.get('/posts')
-def posts():
-    context = {
-        'page': 'posts'
-    }
-    return render_template('posts/index.html', **context)
+# @app.get('/blog')
+# def blog_home():
+#     page = request.args.get('p', 1)
+#     query = request.args.get('q', '')
+#     posts = get_posts('./posts', (int(page) - 1) * POST_COUNT_PER_PAGE + 1, POST_COUNT_PER_PAGE, query)
+#     context = {
+#         'page': 'blog',
+#         'posts': posts,
+#     }
+#     return render_template('posts/index.html', **context)
 
-@app.get('/posts/<path:slug>')
+@app.get('/blog/<path:slug>')
 def post(slug: str):
     filename = f'./posts/{slug}.md'
-    last_modified = os.path.getmtime(filename)
     post = frontmatter.load(filename)
-    post.metadata['last_modified'] = arrow.get(last_modified).to('Asia/Jakarta').format('DD/MM/YYYY HH:mm')
     context = {
-        'page': 'posts',
+        'page': 'blog',
         'meta': post.metadata,
         'content': post.content
     }
--- posts/hello.md
+++ posts/hello.md
@@ -1,5 +1,5 @@
 ---
-title: Hello!
+title: Lorem ipsum dolor sit amet consectetur, adipisicing elit.!
 description: My first post in this blog.
 excerpt: This is the excerpt.
 keywords: hello world 
@@ -7,10 +7,8 @@ keywords: hello world
 date: 27/03/2023
 ---
 
-# Hello there
-
+## Hello word  
 This is hello world
-
 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti fugiat repellat fugit asperiores dolorem incidunt, excepturi harum blanditiis assumenda, eos dolores reiciendis voluptates error ut, sunt doloribus omnis! Totam laboriosam, beatae obcaecati vitae accusantium odio recusandae, necessitatibus nemo unde laborum quod quia molestias perferendis qui in consequatur repudiandae quae deserunt velit architecto voluptatum saepe perspiciatis. Eaque a itaque vel, tenetur, velit quia sapiente qui fuga omnis iure laborum cum. A quis sint repellendus! Maxime autem facilis totam, officia quos repellendus et ipsum minima accusamus, excepturi aliquam nemo quisquam magnam modi fuga similique minus temporibus ullam? Adipisci suscipit voluptates aspernatur ipsum harum blanditiis itaque. Commodi est quibusdam accusantium aliquid hic mollitia, ipsam tempore iusto fugit a eum reprehenderit ullam neque, ut id optio incidunt? Dolorem eaque eius, necessitatibus cupiditate perspiciatis explicabo sit alias ullam reprehenderit rerum quibusdam, labore beatae hic harum accusantium nostrum quasi unde et adipisci illo repellendus earum quo eum ratione? A corrupti magni deleniti nam nobis perferendis at id explicabo totam officiis tempora iure quis, enim sit velit reprehenderit ratione esse modi similique in ullam delectus cumque facilis! Harum recusandae autem nihil, officiis possimus dicta earum nemo voluptates ut ab quod quasi blanditiis, inventore mollitia incidunt a odio.
 
 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti fugiat repellat fugit asperiores dolorem incidunt, excepturi harum blanditiis assumenda, eos dolores reiciendis voluptates error ut, sunt doloribus omnis! Totam laboriosam, beatae obcaecati vitae accusantium odio recusandae, necessitatibus nemo unde laborum quod quia molestias perferendis qui in consequatur repudiandae quae deserunt velit architecto voluptatum saepe perspiciatis. Eaque a itaque vel, tenetur, velit quia sapiente qui fuga omnis iure laborum cum. A quis sint repellendus! Maxime autem facilis totam, officia quos repellendus et ipsum minima accusamus, excepturi aliquam nemo quisquam magnam modi fuga similique minus temporibus ullam? Adipisci suscipit voluptates aspernatur ipsum harum blanditiis itaque. Commodi est quibusdam accusantium aliquid hic mollitia, ipsam tempore iusto fugit a eum reprehenderit ullam neque, ut id optio incidunt? Dolorem eaque eius, necessitatibus cupiditate perspiciatis explicabo sit alias ullam reprehenderit rerum quibusdam, labore beatae hic harum accusantium nostrum quasi unde et adipisci illo repellendus earum quo eum ratione? A corrupti magni deleniti nam nobis perferendis at id explicabo totam officiis tempora iure quis, enim sit velit reprehenderit ratione esse modi similique in ullam delectus cumque facilis! Harum recusandae autem nihil, officiis possimus dicta earum nemo voluptates ut ab quod quasi blanditiis, inventore mollitia incidunt a odio.
